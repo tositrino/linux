da99f41ef96efd4c7344d3d5fe07e792a831e9e2
ths <ths@hanskult.de>
2014-08-05 08:46:23 +0200

    created v3.15.8-ths

--- ADDONS/tools/testversion.c
+++ ADDONS/tools/testversion.c
@@ -9,8 +9,8 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-#define UTS_RELEASE        "3.15.6"
-#define THS_RELEASE        "2014-07-26"
+#define UTS_RELEASE        "3.15.8"
+#define THS_RELEASE        "2014-08-05"
 
 
 const char *cbanner_data[]  = {
@@ -18,7 +18,7 @@ const char *cbanner_data[]  = {
 
                                //12345678901234567890123456789012345678901234567890123456789012345678901234567890
                                 "88888 8   8 888888                              eeea     ea  eeee    eeea    \n"
-                                "  8   8   8 8        e   e  eeee eeeee  eeeee      8    ee8  8       8       \n"
+                                "  8   8   8 8        e   e  eeee eeeee  eeeee      8    ee8  8       8  8    \n"
                                 "  8e  8eee8 8eeeee   8   8  8    8   8  8   8    ee8      8  8888    8888    \n"
                                 "  88  88  8     88   8eee8e 8eee 8eee8e 8e  8     e8      8    88    8  8    \n"
                                 "  88  88  8 e   88   88   8 88   88   8 88  8   8e88    8888 8888    8888    \n"
@@ -27,7 +27,7 @@ const char *cbanner_data[]  = {
                                 "ths kernel release " THS_RELEASE " / linux-" UTS_RELEASE "\n",
 
                                 "e   e                                 eeea     ee  eeee    eeea o8o  db  o8o \n"
-                                "8   8   eeee eeeee  eeeee eeee e         8    eea  8       8    o8888db8888o \n"
+                                "8   8   eeee eeeee  eeeee eeee e         8    eea  8       8  8 o8888db8888o \n"
                                 "8eee8e  8    8   8  8   8 8    8       ee8      8  8888    8888     odbo     \n"
                                 "88   8  8eee 8eee8e 8e  8 8eee 8e       e8      8    88    8  8 o88o db o88o \n"
                                 "88   8  88   88   8 88  8 88   88     8e88    8888 8888    8888 oo   db   oo \n"
@@ -66,7 +66,7 @@ const char *cbanner_data[]  = {
 const char *cbanner_1      = "\n"
                                //12345678901234567890123456789012345678901234567890123456789012345678901234567890
                                 "88888 8   8 888888                              eeea     ea  eeee    eeea    \n"
-                                "  8   8   8 8        e   e  eeee eeeee  eeeee      8    ee8  8       8       \n"
+                                "  8   8   8 8        e   e  eeee eeeee  eeeee      8    ee8  8       8  8    \n"
                                 "  8e  8eee8 8eeeee   8   8  8    8   8  8   8    ee8      8  8888    8888    \n"
                                 "  88  88  8     88   8eee8e 8eee 8eee8e 8e  8     e8      8    88    8  8    \n"
                                 "  88  88  8 e   88   88   8 88   88   8 88  8   8e88    8888 8888    8888    \n"
@@ -75,7 +75,7 @@ const char *cbanner_1      = "\n"
 
 const char *cbanner_2      = "\n"
                                 "e   e                                 eeea     ee  eeee    eeea o8o  db  o8o \n"
-                                "8   8   eeee eeeee  eeeee eeee e         8    eea  8       8    o8888db8888o \n"
+                                "8   8   eeee eeeee  eeeee eeee e         8    eea  8       8  8 o8888db8888o \n"
                                 "8eee8e  8    8   8  8   8 8    8       ee8      8  8888    8888     odbo     \n"
                                 "88   8  8eee 8eee8e 8e  8 8eee 8e       e8      8    88    8  8 o88o db o88o \n"
                                 "88   8  88   88   8 88  8 88   88     8e88    8888 8888    8888 oo   db   oo \n"
